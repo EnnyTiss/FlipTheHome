a7319615c2328dc3f8ce82e81deea8d082233f1b
enny <enny@enny-pc>
2025-12-12 11:08:04 +0100

    [BLE detection] fichiers scan .c et .h, ajout dans fichier global flip_the_home.c

--- main/flip_the_home.c
+++ main/flip_the_home.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "ble_scan.h"
 
-void app_main(void)
-{
+void app_main(void) {
+    printf("=== Flip The Home - ESP32-S3 ===\n");
 
+    // Initialize BLE scan
+    ble_scan_init();
+
+    while (1) {
+        ble_scan_run();
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
 }
